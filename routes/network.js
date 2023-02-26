f32fd47d5079e3329dc93582f10d478216b49721
Stephen Smith <96006226+inherentspice@users.noreply.github.com>
2023-02-26 18:47:59 +0000

    Update network.js

--- routes/network.js
+++ routes/network.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const connectionController = require("../controllers/connectionController");
 
 router.post("/createConnection/:id", connectionController.createConnection);
-router.post("/sendRequest/:id",connectionController.sendRequest);
-router.post("/acceptRequest/:id", connectionController.acceptRequest);
-router.post("/declineRequest/:id", connectionController.declineRequest);
+router.put("/sendRequest/:id",connectionController.sendRequest);
+router.put("/acceptRequest/:id", connectionController.acceptRequest);
+router.put("/declineRequest/:id", connectionController.declineRequest);
 router.delete("/deleteFriend/:id", connectionController.deleteFriend);
 
 module.exports = router;
