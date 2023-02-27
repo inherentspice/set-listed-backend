a9abba1d98675ebcfd7900e2b60402b39834fc36
Stephen Smith <stephenlehanesmith@gmail.com>
2023-02-27 13:08:37 +0000

    created getConnections controller

--- routes/network.js
+++ routes/network.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const connectionController = require("../controllers/connectionController");
 
+router.get("/getConnections/:id", connectionController.getConnections);
 router.post("/createConnection/:id", connectionController.createConnection);
 router.put("/sendRequest/:id",connectionController.sendRequest);
 router.put("/acceptRequest/:id", connectionController.acceptRequest);
 router.put("/declineRequest/:id", connectionController.declineRequest);
-router.delete("/deleteFriend/:id", connectionController.deleteFriend);
+router.put("/deleteFriend/:id", connectionController.deleteFriend);
 
 module.exports = router;
