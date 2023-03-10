b17579895918f8d6c3d27a05ae473f1a559f9cae
inherentspice <stephenlehanesmith@gmail.com>
2023-03-10 16:13:10 +0000

    re-wrote getFilteredConnections controller to return list of users that match search param

--- routes/network.js
+++ routes/network.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connectionController = require("../controllers/connectionController");
 
 router.get("/getConnections/:id", connectionController.getConnections);
-router.get("/getFilteredConnections/:id/:searchParam", connectionController.getFilteredConnections);
+router.get("/getFilteredConnections/:searchParam", connectionController.getFilteredConnections);
 router.post("/createConnection/:id", connectionController.createConnection);
 router.put("/sendRequest/:id",connectionController.sendRequest);
 router.put("/acceptRequest/:id", connectionController.acceptRequest);
